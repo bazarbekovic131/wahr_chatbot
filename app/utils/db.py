d869b8c10a0cd8b6efff466344851ad448fcfa77
Jana Bazarbekovic <bazar.akhmet@gmail.com>
2024-07-23 10:09:47 +0500

    debug

--- app/utils/db.py
+++ app/utils/db.py
@@ -124,7 +124,7 @@ class WADatabase():
             cur.execute("UPDATE users SET current_step = current_step + 1 WHERE phone = %s;", (phone,))
             self.conn.commit()
 
-    def increment_step(self, phone):
+    def set_step(self, phone):
         with self.conn.cursor() as cur:
             cur.execute("UPDATE users SET current_step =  1 WHERE phone = %s;", (phone,))
             self.conn.commit()
