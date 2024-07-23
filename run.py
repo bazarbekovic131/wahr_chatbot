91fe8c2996a1abbc29906ff94dcbfafdadf090ae
Jana Bazarbekovic <bazar.akhmet@gmail.com>
2024-07-23 17:47:29 +0500

    debug schedule

--- run.py
+++ run.py
@@ -1,18 +1,28 @@
 import logging
-import threading
 from app import create_app
 from app.services.email_service import mailmain
-
+import schedule
+import threading
+import time
 app = create_app()
 
 def start_mailmain():
-    logging.info("Email service started")
-    mailmain()
+    logging.info("Email service started\n\n\n")
+    # mailmain()
+
+def run_scheduler():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
 
 if __name__ == "__main__":
-    mail_thread = threading.Thread(target=start_mailmain)
-    mail_thread.daemon = True
-    mail_thread.start()
+
+    schedule.every(5).seconds.do(start_mailmain) # try to schedule
+    scheduler_thread = threading.Thread(target=run_scheduler)
+    scheduler_thread.daemon = True
+    scheduler_thread.start()
 
     logging.info("Flask app started")
     app.run(host="127.0.0.1", port=5000)
